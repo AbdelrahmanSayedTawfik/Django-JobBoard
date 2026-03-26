50cee3fc956bd16334e4d0c80baec02d8656a6d1
AbdelrahmanSayedTawfik <abdelrahmansayed1722@gmail.com>
2026-03-26 17:54:50 +0200

    make tables in db sqlite3 and work in admin panal

--- job/models.py
+++ job/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+JOB_TYPE = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+)
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=100, choices=JOB_TYPE, default='Full Time')
+    description = models.TextField(default='')
+    published_at = models.DateTimeField(auto_now=True)
+    vacency = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experance = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return self.title

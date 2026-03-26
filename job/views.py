69b66e9eaf405e2d5961f8e6597feb0a2e40297b
AbdelrahmanSayedTawfik <abdelrahmansayed1722@gmail.com>
2026-03-26 22:42:28 +0200

    work on views and models and urls

--- job/views.py
+++ job/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def job_list(request):
+    #return render(request , 'job/job_list.html')
+    pass
+
+def job_detail(request,id):
+    #return render(request , 'job/job_detail.html')
+    pass
